cf2933f0f144432c7e1d631659d8fa2f067ea664
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2013-10-31 23:16:04 -0700

    Create one to many relationship between User and Post models.  Add 'user_id' to Posts migrations.  Reset the db.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,11 +6,11 @@ class PostsController < ApplicationController
   end
   
   def new
-    @post = Post.new
+    @post = current_user.posts.new
   end
   
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.new(post_params)
  
     if @post.save
       redirect_to @post
@@ -20,15 +20,15 @@ class PostsController < ApplicationController
   end
   
   def show
-    @post = Post.find(params[:id])
+    @post = current_user.posts.find(params[:id])
   end
   
   def edit
-    @post = Post.find(params[:id])
+    @post = current_user.posts.find(params[:id])
   end
   
   def update
-    @post = Post.find(params[:id])
+    @post = current_user.posts.find(params[:id])
  
     if @post.update(post_params)
       redirect_to @post
@@ -38,7 +38,7 @@ class PostsController < ApplicationController
   end
   
   def destroy
-    @post = Post.find(params[:id])
+    @post = current_user.posts.find(params[:id])
     @post.destroy
     
     redirect_to posts_path
