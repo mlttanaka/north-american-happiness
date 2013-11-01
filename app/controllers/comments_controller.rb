2363a94bfac92c1e1c52f81bcd6122fb40295bc5
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2013-10-31 22:13:31 -0700

    Move comment parameters for create method in Comments controller to private methond.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment].permit(:commenter, :body))
+    @comment = @post.comments.create(comment_params)
     
     redirect_to post_path(@post)
   end
@@ -13,6 +13,13 @@ class CommentsController < ApplicationController
     @comment.destroy
     
     redirect_to post_path(@post)
+  end
+  
+  
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
   end
   
 end
